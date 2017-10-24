f4ac465724186a51f89faed76f652bd531cda35c
cruisecoders <info.mrjsingh@gmail.com>
2017-10-24 16:04:43 +0530

    angular4-table-demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  list:any = [{
+	name: 'Jon', 
+	joining_date:'23/10/2015', 
+	age: 23
+   },
+   {
+	name:'Viki', 
+	joining_date:'24/01/2015', 
+	age: 20
+   },
+   {
+	name: 'Abc', 
+	joining_date:'25/10/2015', 
+	age: 43
+   },
+   {
+	name: 'XYZ', 
+	joining_date:'28/10/2015', 
+	age: 21
+   }
+   ];
+   
+  
+   
+
 }
